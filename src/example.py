07ddb16f7087223976bf167b750e6cf7a6eb7ce3
joelsonolissis <olissismorais1@outlook.com>
2021-02-17 17:18:57 +0000

    second commit - exibir informacoes da imagem

--- src/example.py
+++ src/example.py
@@ -1,9 +1,17 @@
 import numpy as np
 import cv2
+def showImage(img):
+    from matplotlib import pyplot as plt
 
-obj_img = cv2.imread("img/joe.jfif")
-from matplotlib import pyplot as plt
-obj_img = cv2.cvtColor(obj_img, cv2.COLOR_BGR2RGB)
-plt.imshow(obj_img)
-plt.show()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.show()
 
+def main():
+    obj_img = cv2.imread("img/joe.jfif")
+    altura, largura, canais_de_cor = obj_img.shape
+    print("Dimensoes da Imagem " + str(largura) + "," + str(altura))
+    print("Canais de cor: ", canais_de_cor)
+    showImage(obj_img)
+
+main()
